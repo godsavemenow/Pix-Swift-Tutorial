79155d7006f99c536144fbc812bd5a650850fd38
godsavemenow <lvjs@cin.ufpe.br>
2021-04-29 20:32:17 -0300

    Initial Commit

---
+++ Pix-Swift-Tutorial/Pix-Swift-Tutorial/Pix_Swift_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pix_Swift_TutorialApp.swift
+//  Pix-Swift-Tutorial
+//
+//  Created by Lucas Silva on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Pix_Swift_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
